ce52bb9ddb410608efd807ce04c2d34521af82d1
deepakyadav121999 <dy44444221@gmail.com>
2023-01-17 14:23:54 +0530

    fpp

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+import Heading from './Heading.js;';
+import Heading from './InputQuery.js;';
+import Heading from './SubHeading.js;';
+import Heading from './SubmitButton.js;';
+
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <InputQuery/>
+      <SubHeading/>
+      <SubmitButton/>
     </div>
   )
 }
+export default App;
 
 
